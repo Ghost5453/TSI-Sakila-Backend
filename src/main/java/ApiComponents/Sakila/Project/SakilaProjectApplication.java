87642c8c067c197a5ec5af4fc3e3d6051f5bcbfb
Ghost5454 <adam1petchey@gmail.com>
2022-10-31 17:07:20 +0000

    Added actor post mapping

--- src/main/java/ApiComponents/Sakila/Project/SakilaProjectApplication.java
+++ src/main/java/ApiComponents/Sakila/Project/SakilaProjectApplication.java
@@ -29,25 +29,25 @@ public class SakilaProjectApplication {
 	}
 
 	//region GetMappings
-	@GetMapping("/actor")
+	@GetMapping("/actors")
 	public @ResponseBody
 	Iterable<Actor> getAllActors(){
 		return actorRepo.findAll();
 	}
 
-	@GetMapping("/actor/{id}")
+	@GetMapping("/actors/{id}")
 	public Actor getSignalActor(@PathVariable(value = "id") int actorID)
 	{
 		return actorRepo.findById(actorID).orElseThrow(() -> new ResourceAccessException("Actor not found at index " + actorID));
 	}
 
-	@GetMapping("/address")
+	@GetMapping("/addresses")
 	public @ResponseBody
 	Iterable<Address> getAllAddresses(){
 		return addressRepo.findAll();
 	}
 
-	@GetMapping("/address/{id}")
+	@GetMapping("/addresses/{id}")
 	public Address getSignalAddress(@PathVariable(value = "id") int addressID)
 	{
 		return  addressRepo.findById(addressID).orElseThrow(() -> new ResourceAccessException("Address not found at index " + addressID));
@@ -55,7 +55,7 @@ public class SakilaProjectApplication {
 	//endregion
 
 	//region PutMappings
-	@PutMapping("/actor/{id}")
+	@PutMapping("/actors/{id}")
 	public ResponseEntity<Actor> updateActor(@PathVariable(value = "id") int actorID, @Validated @RequestBody Actor actorDetails)
 			throws ResourceAccessException {
 		Actor actor = actorRepo.findById(actorID)
@@ -74,6 +74,15 @@ public class SakilaProjectApplication {
 //		Address address = addressRepo.findById(addressDetails.getAddressID()).orElseThrow(() -> new ResourceAccessException("Address not found for this id :: " + addressID));
 //
 //	}
+	//endregion
+
+	//region PostMapping
+	@PostMapping("/actors")
+	public Actor createActor(@Validated @RequestBody Actor actor)
+	{
+		return actorRepo.save(actor);
+	}
+
 	//endregion
 
 }
